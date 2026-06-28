ae6cd4b653d9cc6e2050069f99f7ee084dbaf2fc
Zayan-Sheikh <160139662+zyanshykh@users.noreply.github.com>
2026-06-29 01:40:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const skillsList = document.getElementById('skills-list');
 toggleButton.addEventListener('click', () => {
     if (skillsList.style.display === 'none') {
         skillsList.style.display = 'block';
+        toggleButton.textContent = 'Hide Skills'; 
     } else {
         skillsList.style.display = 'none';
+        toggleButton.textContent = 'Show Skills'; 
     }
 });

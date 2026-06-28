e177111530171ed0699063e89551f5005b0b4484
Zayan-Sheikh <160139662+zyanshykh@users.noreply.github.com>
2026-06-29 01:39:25 +0500

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,11 +1,6 @@
-// script.ts
 const toggleButton = document.getElementById('toggle-Skills') as HTMLButtonElement;
 const skillsList = document.getElementById('skills-list') as HTMLElement;
 
 toggleButton.addEventListener('click', () => {
-    if (skillsList.style.display === 'none') {
-        skillsList.style.display = 'block';
-    } else {
-        skillsList.style.display = 'none';
-    }
+    skillsList.style.display = (skillsList.style.display === 'none') ? 'block' : 'none';
 });
